de4eb84bdefe8fb52383ab71073f79d228cf4e95
Ryan Gendel <ryangendel@Ryans-MacBook-Pro.local>
2024-06-17 19:05:20 -0600

    not sure what I changed

--- src/utils/REIContext.js
+++ src/utils/REIContext.js
@@ -5,7 +5,7 @@ const REIContext = createContext()
 export const useREIContext = () => useContext(REIContext)
 
 export const REIContextProvider = ( {children} ) =>{
-    const [bikeInventory, setBikeInventory]= useState([{},{},{}])
+    const [bikeInventory, setBikeInventory]= useState([{name:"ryan", id:123, balance:122},{},{}])
     const [userInfo, setUserInfo]= useState({name:'Ryan', cc:12345678})
     //
 
