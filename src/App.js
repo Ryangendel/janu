ff0a162f3094a7916da15039b51208131c1e79c0
Ryan Gendel <ryangendel@Ryans-MacBook-Pro.local>
2024-06-24 19:22:43 -0600

    message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import { useState, useEffect } from "react"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {BrowserRouter as Zoo, Routes, Route} from "react-router-dom"
 import Home from "./Home"
 import AboutUs from "./AboutUs"
 import Inventory from "./Inventory"
@@ -12,16 +12,15 @@ function App() {
 
   return (
     <div className="router">
-      <REIContextProvider>
-        {1+1==3?<h1>way to go</h1>:<h1>youre wrong</h1>}
-     <Router>
+    <REIContextProvider>
+     <Zoo>
       <Routes>
         <Route path="/home" element={<Home />}/>
         <Route path="/aboutus" element={<AboutUs />}/>
         <Route path="/inventory" element={<Inventory />}/>
         <Route path="/inventory/:productId/:productName" element={<InventorySingle />}/>
       </Routes>
-     </Router>
+     </Zoo>
      </REIContextProvider>
     </div>
   );
